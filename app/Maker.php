f7d5b86f03c8ee3bbc609e2fe4137feb33a51491
takanoakira <soraboruta@gmail.com>
2019-07-25 06:46:14 +0000

    CastsEnumsのエイリアスを作成

--- app/Maker.php
+++ app/Maker.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use App\Enums\PrefectureCode;
+use BenSampo\Enum\Traits\CastsEnums;
 use Illuminate\Database\Eloquent\Model;
 
 
 class Maker extends Model
 {
+    use CastsEnums;
+    
      protected $dates = [
         'created_at',
         'updated_at'
