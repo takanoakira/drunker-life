281d2572a6c6aa69876725ba4dd759f412b72723
takanoakira <soraboruta@gmail.com>
2019-07-26 05:46:07 +0000

    お酒テーブルの項目を追加

--- database/migrations/2019_07_25_160342_create_liquors_table.php
+++ database/migrations/2019_07_25_160342_create_liquors_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Enums\PrefectureCode;
 
 class CreateLiquorsTable extends Migration
 {
@@ -15,6 +16,16 @@ class CreateLiquorsTable extends Migration
     {
         Schema::create('liquors', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('maker_id');
+            $table->integer('price');
+            $table->integer('alcohol');
+            $table->integer('acidity');
+            $table->integer('liquor_score');
+            $table->enum('production_area', PrefectureCode::getValues());
+            $table->string('raw_rice');
+            $table->integer('milling_rate');
+            $table->text('detail');
             $table->timestamps();
         });
     }
