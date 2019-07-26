ff384ffd44f9f3d79224a2e7445e814b5d24ae72
takanoakira <soraboruta@gmail.com>
2019-07-26 06:57:47 +0000

    モデルにprefecturecodeの自動キャストを追加

--- app/Liquor.php
+++ app/Liquor.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Enums\PrefectureCode;
 use Illuminate\Database\Eloquent\Model;
+use BenSampo\Enum\Traits\CastsEnums;
 
 class Liquor extends Model
 {
-    //
+       use CastsEnums;
+    
+     protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+ 
+    protected $enumCasts = [
+        'production_area' => PrefectureCode::class,
+    ];
 }
