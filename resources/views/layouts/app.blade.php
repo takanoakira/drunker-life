ad2c8966fc43ed1e4c3e603036ce32b8fe45fc5e
takanoakira <soraboruta@gmail.com>
2019-08-10 04:32:18 +0000

    タブ部分にお酒一覧と販売元一覧へのリンクを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,14 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                 <div class="links">
+                    <a class="navbar-brand" href="{{ url('makers/') }}" >
+                        {{ __('makers_index') }}
+                    </a>
+                    <a class="navbar-brand" href="{{ url('liquors/') }}">
+                        {{ __('liquors_index') }}
+                    </a>
+                </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
