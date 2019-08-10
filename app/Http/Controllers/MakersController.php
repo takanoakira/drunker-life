b43a213ed53321944d486a32532e08c143a67ca8
takanoakira <soraboruta@gmail.com>
2019-08-10 04:15:58 +0000

    一覧ページと詳細ページ以外に認証機能を追加

--- app/Http/Controllers/MakersController.php
+++ app/Http/Controllers/MakersController.php
@@ -7,6 +7,10 @@ use App\Maker;
 
 class MakersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index','show']);
+    }
     /**
      * Display a listing of the resource.
      *
