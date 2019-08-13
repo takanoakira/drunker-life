aab4e3e771ab7b060d6b31b726e41a29756a4ba3
takanoakira <soraboruta@gmail.com>
2019-08-13 05:56:31 +0000

    検索フォーム、検索機能を追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,39 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <!--↓↓ 検索フォーム ↓↓-->
+    <div class="col-md-3">   
+        <form class="form-inline" method="get">
+            <div class="form-group">
+                {{Form::label('keyword','キーワード')}}
+                {{Form::text('keyword', null,  ['class' => 'form-control', 'autofocus','placeholder' => "お酒名か販売元名を入力"])}}
+                {{Form::label('production_area','産地')}}
+                {{Form::select('production_area', (array('' => '--') + \App\Enums\PrefectureCode::toSelectArray()), $production_area)}}  
+                <input type="submit" value="検索" >
             </div>
-        </div>
+        </form>
+    </div>
+    <!--↑↑ 検索フォーム ↑↑-->
+    
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>お酒名</th>
+                    <th>産地</th>
+                    <th>販売元名</th>
+                </tr>
+            </thead>
+            <tbody>
+                 @foreach ($liquors as $liquor)
+                    <tr>
+                        <td><a href="{{ url('liquors/'.$liquor->id) }}">{{ $liquor->name }}</a></td>
+                        <td>{{ $liquor->production_area->description }}</td>
+                        <td><a href="{{ url('liquors/'.$liquor->maker_id) }}">{{ $liquor->maker->name }}</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
