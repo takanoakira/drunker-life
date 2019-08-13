2410d0be827bd0b9a555072596f2efd62ce838ef
takanoakira <soraboruta@gmail.com>
2019-08-13 05:55:07 +0000

    お酒名、販売元名、産地での検索機能を追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class HomeController extends Controller
 {
     /**
@@ -17,12 +18,33 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+         //キーワードを取得
+        $keyword = $request->input('keyword');
+        $production_area = $request->input('production_area');
+        $query = \App\Liquor::select();
+        
+        if(!empty($keyword))
+        {   
+            //お酒名から検索
+            $query = $query->where('name', 'like', '%'.$keyword.'%')->orWhereHas('maker', function($q) use ($keyword) {
+                $q->where('name', 'like','%'.$keyword.'%');
+            });
+        }
+        
+         if(!empty($production_area))
+        {   
+            //産地から検索
+            $query = $query->where('production_area', $production_area);
+            
+        }
+        
+        $query = $query->get();
+        return view('home',['liquors' => $query,'keyword' => $keyword,'production_area' => $production_area]);
     }
 }
