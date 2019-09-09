12bf00059aa9eef984968780ee5979bd050f279c
takanoakira <soraboruta@gmail.com>
2019-09-09 05:42:17 +0000

    上部のタブにtagの一覧ページへのリンクを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,9 @@
                     <a class="navbar-brand" href="{{ url('liquors/') }}">
                         {{ __('liquors_index') }}
                     </a>
+                     <a class="navbar-brand" href="{{ url('tags/') }}">
+                        {{ __('tags_index') }}
+                    </a>
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
