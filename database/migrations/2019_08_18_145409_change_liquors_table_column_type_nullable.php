bb9f969558632dc80957b6bcf831369a628f0bb2
takanoakira <soraboruta@gmail.com>
2019-09-09 05:52:51 +0000

    マイグレーションファイル名が重複していたためファイル名を変更。

---
+++ database/migrations/2019_08_18_145409_change_liquors_table_column_type_nullable.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeLiquorsTableColumnTypeNullable extends Migration
+{
+     public function __construct()
+    {
+        DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('liquors', function (Blueprint $table) {
+            $table->decimal('alcohol', 4, 1)->nullable()->change();
+            $table->decimal('acidity', 4, 1)->nullable()->change();
+            $table->decimal('liquor_score', 5, 1)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('liquors', function (Blueprint $table) {
+            $table->decimal('alcohol', 4, 1)->nullable(false)->change();
+            $table->decimal('acidity', 4, 1)->nullable(false)->change();
+            $table->decimal('liquor_score', 5, 1)->nullable(false)->change();
+        });
+    }
+}
