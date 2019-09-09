a63b03346276dc72f0abd5efbed1b4ada647d359
takanoakira <soraboruta@gmail.com>
2019-09-09 05:51:07 +0000

    Tagコントローラーを追加。CRUD機能を追加。

---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tag;
+use Illuminate\Http\Request;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::select();
+        
+        $tags = $tags->orderBy('created_at','desc')->paginate(10);
+        return view('tags.index',['tags' => $tags]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tag = new Tag;
+        $tag->name =$request->name;
+        $tag->save();
+        
+        return redirect('tags/'.$tag->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tag $tag)
+    {
+        return view('tags.show',['tag' => $tag]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tag $tag)
+    {
+       return view('tags.edit',['tag' => $tag]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        $tags = new Tag;
+        $tag->name =$request->name;
+        return redirect('tags/'.$tags->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        return redirect('tags');
+    }
+}
